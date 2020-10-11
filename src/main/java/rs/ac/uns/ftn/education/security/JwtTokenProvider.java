df8ae7cc3c26f48950c3f1ee4adf64241d7cff69
Marko Krizan <markokrizan64@gmail.com>
2020-10-11 20:27:26 +0200

    Add roles to jwt claim

--- src/main/java/rs/ac/uns/ftn/education/security/JwtTokenProvider.java
+++ src/main/java/rs/ac/uns/ftn/education/security/JwtTokenProvider.java
@@ -27,19 +27,13 @@ public class JwtTokenProvider {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
 
-        return Jwts.builder()
-                .setSubject(Long.toString(userPrincipal.getId()))
-                .setIssuedAt(new Date())
-                .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
+        return Jwts.builder().setSubject(Long.toString(userPrincipal.getId())).setIssuedAt(new Date())
+                .setExpiration(expiryDate).claim("roles", userPrincipal.getAuthorities())
+                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
     }
 
     public Long getUserIdFromJWT(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
 
         return Long.parseLong(claims.getSubject());
     }
