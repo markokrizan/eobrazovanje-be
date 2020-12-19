7ad5e99f9252d62c14d5bea707e529aaccc033fc
Marko Krizan <markokrizan64@gmail.com>
2020-12-19 22:37:22 +0100

    Add espb points and study program type to study program request

--- src/main/java/rs/ac/uns/ftn/education/payload/StudyProgramRequest.java
+++ src/main/java/rs/ac/uns/ftn/education/payload/StudyProgramRequest.java
@@ -1,10 +1,12 @@
 package rs.ac.uns.ftn.education.payload;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
 import rs.ac.uns.ftn.education.model.Course;
+import rs.ac.uns.ftn.education.model.StudyProgramType;
 import lombok.NoArgsConstructor;
 import java.util.Set;
 
@@ -21,6 +23,12 @@ public class StudyProgramRequest {
 
   @NotBlank
   private String studyField;
+
+  @NotNull
+  private Integer espbPoints;
+
+  @NotNull
+  private StudyProgramType studyProgramType;
 
   Set<Course> courses; 
 }
