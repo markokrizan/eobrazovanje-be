d3602421f6102491154bf8e9919a26e54b336c11
Marko Krizan <markokrizan64@gmail.com>
2021-01-09 15:29:38 +0100

    Add study year to student request

--- src/main/java/rs/ac/uns/ftn/education/payload/StudentRequest.java
+++ src/main/java/rs/ac/uns/ftn/education/payload/StudentRequest.java
@@ -6,10 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import rs.ac.uns.ftn.education.model.StudyProgram;
+import rs.ac.uns.ftn.education.model.Year;
 
 @Getter @Setter @NoArgsConstructor
 public class StudentRequest extends UserRequest {
 
   @NotNull
   private StudyProgram studyProgram;
+
+  @NotNull
+  private Year currentStudyYear;
 }
