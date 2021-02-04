9d5d7b41ffac2bf5d9810f0066daaa7f876ca7a2
Marko Krizan <markokrizan64@gmail.com>
2021-02-04 19:51:05 +0100

    Engagements unique constraint

--- src/main/java/rs/ac/uns/ftn/education/model/Engagement.java
+++ src/main/java/rs/ac/uns/ftn/education/model/Engagement.java
@@ -1,5 +1,6 @@
 package rs.ac.uns.ftn.education.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -16,7 +18,10 @@ import lombok.NoArgsConstructor;
 import java.util.Objects;
 
 @Entity
-@Table(name = "engagements")
+@Table(
+  name = "engagements",
+  uniqueConstraints = @UniqueConstraint(columnNames={"engagement_type", "course_id", "teacher_id", })
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -26,6 +31,7 @@ public class Engagement {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="engagement_type")
     private EngagementType engagementType;
 
     @ManyToOne
